879ddfc30534f2fad9648f5cda85b221e7e6ae9c
benardopiyo <benardopiyo13@gmail.com>
2024-02-07 16:34:19 +0300

    Deleted the comments and checked on the warnings

--- lib/mpesa.ex
+++ lib/mpesa.ex
@@ -4,43 +4,30 @@ defmodule Mpesa do
     @initial_bal + deposit_amount
   end
 
-  # make_withdrawal
   def make_withdrawal(withdrawal_amount) when @initial_bal > withdrawal_amount do
 
     @initial_bal - withdrawal_amount
   end
 
-  def make_withdrawal(withdrawal_amount) do
+  def make_withdrawal(_withdrawal_amount) do
     "unlimited funds"
   end
 
-  # defp enter_agentNo(num_agent) do
-  #   int(num_agent)
-  # end
-
-  # defp enter_storeNo(num_store) do
-  #   int(num_store)
-  # end
-
-  # send_money
   def send_money(send_amount) when @initial_bal > send_amount do
     @initial_bal - send_amount
   end
 
-  def send_money(send_amount) do
+  def send_money(_send_amount) do
     "unlimited funds"
   end
 
-  # buy_airtime
+
   def buy_airtime(initial_bal, airtime_amount) when initial_bal > airtime_amount do
     initial_bal - airtime_amount
   end
 
-  def buy_airtime(initial_bal, airtime_amount) do
+  def buy_airtime(_initial_bal, _airtime_amount) do
     "unsuccessful purchase"
   end
 
-  # def enter_receiverNo(num_receiver) do
-  #   int(num_receiver)  #10 digit integer
-  # end
 end
