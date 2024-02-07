c15937ceb08d58034b6a474200ab57bc036f4550
benardopiyo <benardopiyo13@gmail.com>
2024-02-07 15:10:14 +0300

    Implemented and tested withdrawal, deposit, and send money functionalities in MPESA project

--- lib/mpesa.ex
+++ lib/mpesa.ex
@@ -1,18 +1,46 @@
 defmodule Mpesa do
-  @moduledoc """
-  Documentation for `Mpesa`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Mpesa.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @initial_bal 1000
+  def make_deposit(deposit_amount) do
+    @initial_bal + deposit_amount
   end
+
+  # make_withdrawal
+  def make_withdrawal(withdrawal_amount) when @initial_bal > withdrawal_amount do
+
+    @initial_bal - withdrawal_amount
+  end
+
+  def make_withdrawal(withdrawal_amount) do
+    "unlimited funds"
+  end
+
+  # defp enter_agentNo(num_agent) do
+  #   int(num_agent)
+  # end
+
+  # defp enter_storeNo(num_store) do
+  #   int(num_store)
+  # end
+
+  # send_money
+  def send_money(send_amount) when @initial_bal > send_amount do
+    @initial_bal - send_amount
+  end
+
+  def send_money(send_amount) do
+    "unlimited funds"
+  end
+
+  # buy_airtime
+  def buy_airtime(initial_bal, airtime_amount) when initial_bal > airtime_amount do
+    initial_bal - airtime_amount
+  end
+
+  def buy_airtime(initial_bal, airtime_amount) do
+    "unsuccessful purchase"
+  end
+
+  # def enter_receiverNo(num_receiver) do
+  #   int(num_receiver)  #10 digit integer
+  # end
 end
--- test/mpesa_test.exs
+++ test/mpesa_test.exs
@@ -2,7 +2,39 @@ defmodule MpesaTest do
   use ExUnit.Case
   doctest Mpesa
 
-  test "greets the world" do
-    assert Mpesa.hello() == :world
+  test "make_withdrawal/1 fails if withdrawal amount is more than initial bal" do
+    assert Mpesa.make_withdrawal(2000) == "unlimited funds"
   end
+
+  test "make_withdrawal/1 fails if withdrawal amount is equal to initial bal" do
+    assert Mpesa.make_withdrawal(1000) == "unlimited funds"
+  end
+
+  test "make_withdrawal/1 is successful if withdrawal amount is less than initial bal" do
+    assert Mpesa.make_withdrawal(200) == 800
+  end
+
+    test "send_money/1 fails if send amount is more than initial bal" do
+      assert Mpesa.send_money(3000) == "unlimited funds"
+    end
+
+    test "send_money/1 fails if send amount is equal to initial bal" do
+      assert Mpesa.send_money(1000) == "unlimited funds"
+    end
+
+    test "send_money/1 is successful if send amount is less than initial bal" do
+      assert Mpesa.send_money(400) == 600
+    end
+
+    test "make_deposit/1 successful if deposit amount is more than initial bal" do
+      assert Mpesa.make_deposit(1500) == 2500
+    end
+
+    test "make_deposit/1 successful if deposit amount is less than initial bal" do
+      assert Mpesa.make_deposit(500) == 1500
+    end
+
+    test "make_deposit/1 successful if deposit amount is equal to initial bal" do
+      assert Mpesa.make_deposit(1000) == 2000
+    end
 end
